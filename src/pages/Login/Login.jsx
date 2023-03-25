0548a00262e9194e8193d5a03911395bf652db93
CesarNartam <213473@ids.upchiapas.edu.mx>
2023-03-25 14:59:20 -0600

    Button component update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,11 @@
+import Button from "../../components/Button/Button";
+
 function Login() {
     return (
         <h1 className='pb-2 text-4xl'>Inicia Sesión</h1>
+        
     );
+    <Button/>
 }
 
 export default Login;
