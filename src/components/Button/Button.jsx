b65fe1cc7ff266792e0f8ce303673f17fe437682
Mauricio Cruz <mawcp7@gmail.com>
2023-03-26 12:05:03 -0600

    improve button and input components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ width, type, children }) => {
-    return (<button type={type} className={`${width} bg-primary-blue text-white font-light text-sm rounded-md px-4 py-2`}>
+    return (<button type={type} className={`${width} h-10 bg-primary-blue text-white font-light text-sm rounded-md px-4 py-2`}>
         {children}
     </button>
     )
