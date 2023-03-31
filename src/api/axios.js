c7c383b72bc5d1a8f820bd9e3a12f52d5a3400ad
edualt <eduardosebastianhdz503@gmail.com>
2023-03-30 21:31:55 -0600

    feat(api): add new api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const axiosAPIBridgeInstance = axios.create({
-  baseURL: 'http://18.233.176.235:8080/api/',
+  baseURL: 'https://postmenbridge.ddns.net:8443/api/',
 });
 
 export const axiosAPIInstance = axios.create({
-  baseURL: 'http://50.16.139.51:8080/api/'
+  baseURL: 'https://postmen.ddns.net:8443/api/'
 });
